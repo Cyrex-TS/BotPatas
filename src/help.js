9b9ee2059c87531ef2dd07966a3422eb346b38f0
Cyrex-TS <82237807+Cyrex-TS@users.noreply.github.com>
2021-04-16 23:44:31 -0300

    Update help.js

--- src/help.js
+++ src/help.js
@@ -1,14 +1,15 @@
 const help = (prefix) => {
 	return `
-「 *Sr Patas* 」
+「 *YAMA-BOT* 」
 
 ◪ *Información*
   ❏ Comando: 「  ${prefix}  」
-  ❏ Creador: Cyrex
+  ❏ Creador: *Cyrex*
   ❏ Instagram: https://www.instagram.com/cyrex.ofc
+  ❏ ----Seguime ciruja----
 
 ◪ *Para usar al bot*
-  ❏ Registrate con el comando ${prefix}daftar Nombre
+  ❏ Registrate con el comando ${prefix}daftar y tu nombre
   ❏ No romper la pija cebado con los comandos porque sos boleta
 
 ◪ *CREAR*
@@ -31,10 +32,11 @@ const help = (prefix) => {
 ◪ *NSWF*
   │
   └─ ❏ ${prefix}nsfwmenu
+     ❏ Ch ch ey, qué haces pajín? Ojito donde pones la mano eh.. 
 
      ❏ Para activar los NSFW coloque el siguiente comando ${prefix}nsfw1 y para desactivar los NSFW coloque ${prefix}nsfw0
 
-No te olvides de seguirme en instagram cara de pingo
+No te olvides de seguirme en instagram cara de pingo, es lo unico que pido para que usen el bot.
 `
 }
 
